03380245a6b49b1746632e5a15fe184bb3231ab3
Danish Mughal <mughal.danish@gmail.com>
2016-12-12 20:42:25 -0800

    Added create employee functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,4 +5,20 @@ class EmployeesController < ApplicationController
   end
 
 
+  def new
+  end
+
+  def create
+    input_first_name = params[:first_name]
+    input_last_name = params[:last_name]
+    input_email = params[:email]
+
+    @employee = Unirest.post("http://localhost:3001/api/v2/employees", 
+              parameters: {first_name: input_first_name, last_name: input_last_name, email: input_email},
+              headers: {"Accept" => "application/json"}).body
+
+    # redirect_to "/employees/#{@employee['id']}"
+    redirect_to employee_path(@employee["id"])
+  end
+
 end
